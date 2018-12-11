9e7e69a39359853f1911be37bf71ddb77e0aa6c8
Andy hp <gianghnguyen84@gmail.com>
2018-12-11 14:55:15 +0700

    update ui params x2

--- src/main/java/com/mkyong/rest/client/NetClientPost.java
+++ src/main/java/com/mkyong/rest/client/NetClientPost.java
@@ -8,28 +8,72 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+// mk inut12345
 public class NetClientPost {
 
 	// http://localhost:8080/RESTfulExample/json/product/post
 	public static void main(String[] args) {
-
-		sendInutCmd();
+		int hoi1 = 2, hoi2 = 0, delay = 0; // default toggle for 2 sec
+		if(args.length >= 1) {
+			hoi1 = Integer.parseInt(args[0]);
+		}
+		if(args.length == 3) {			
+			hoi2 = Integer.parseInt(args[1]);
+			delay = Integer.parseInt(args[2]);
+		}
 		
+		// Hoi chuong 1st
+		if(hoi1 > 0) {
+			sendInutCmd(); // turn on
+			delaySecond(hoi1);
+			sendInutCmd(); // turn off
+		}
+
+		if(delay > 0) {
+			delaySecond(delay);
+		}
+		
+		if(hoi2 > 0) {
+			sendInutCmd(); // turn on
+			delaySecond(hoi2);
+			sendInutCmd(); // turn off
+		}
+	}
+	
+	private static void delaySecond(int sec) {
 		try {
-			Thread.sleep(8000);
+			Thread.sleep(1000 * sec);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-		sendInutCmd();
-
 	}
 	
+	// Xuong 1 Phu Thi: https://connect.mysmarthome.vn/api/1.0/request/
+	// GmKUmVREXgMdLhVd1o8LgqJazaH2/
+	// H1dTC_Jv7/3ecba306d4200b70e5c2bdb52473255c307024238592/
+	// req_device_toggle
+	// Xuong 2 Phu Tho: https://connect.mysmarthome.vn/api/1.0/request/
+	// GmKUmVREXgMdLhVd1o8LgqJazaH2/
+	// ByaN2SAHX/1ec33704879e21f39292675f3c5d3ba9953049213925/req_device_toggle
+	
 	private static void sendInutCmd() {
+		String[][] urlArr = {
+				{"https://connect.mysmarthome.vn/api/1.0/request/",
+					"GmKUmVREXgMdLhVd1o8LgqJazaH2/ByaN2SAHX/",
+					"1ec33704879e21f39292675f3c5d3ba9953049213925/"}
+				,
+				{"https://connect.mysmarthome.vn/api/1.0/request/",
+					"GmKUmVREXgMdLhVd1o8LgqJazaH2/H1dTC_Jv7/",
+					"3ecba306d4200b70e5c2bdb52473255c307024238592/"}
+				};
+		
 		try {
+			String apiStr = urlArr[0][0]
+					+ urlArr[0][1]
+					+ urlArr[0][2]
+					+ "req_device_toggle";
 
-			URL url = new URL(
-					"https://connect.mysmarthome.vn/api/1.0/request/GmKUmVREXgMdLhVd1o8LgqJazaH2/ByaN2SAHX/18086a6fceea010c175a2bf7ef613248314232166413/req_device_toggle");
+			URL url = new URL(apiStr);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setDoOutput(true);
 			conn.setRequestMethod("POST");
